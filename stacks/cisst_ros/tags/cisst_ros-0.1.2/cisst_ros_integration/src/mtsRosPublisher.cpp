c87fb480002c5b0ab316123bdea92ca6c7d6b128
Jonathan Bohren <jbo@jhu.edu>
2011-06-28 01:14:47 -0400

    [svn r43] Tagging cisst_ros-0.1.2

---
+++ stacks/cisst_ros/tags/cisst_ros-0.1.2/cisst_ros_integration/src/mtsRosPublisher.cpp
@@ -0,0 +1,6 @@
+
+#include <cisstCommon/cmnConstants.h>
+#include "cisst_ros_integration/mtsRosPublisher.h"
+
+// required to implement the class services, see cisstCommon
+CMN_IMPLEMENT_SERVICES(mtsRosPublisher);
