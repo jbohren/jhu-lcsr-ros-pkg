a9d3fcceb83ad06a30f1bf55e62d75c8e917bfd4
Jonathan Bohren <jonathan.bohren+hg@gmail.com>
2011-11-03 17:39:57 -0400

    getting rid of svn garbage

--- stacks/cisst_ros/branches/cisst_ros-pre-0.2.0/cisst_ros_integration/src/mtsRosPublisher.cpp
+++
@@ -1,6 +0,0 @@
-
-#include <cisstCommon/cmnConstants.h>
-#include "cisst_ros_integration/mtsRosPublisher.h"
-
-// required to implement the class services, see cisstCommon
-CMN_IMPLEMENT_SERVICES(mtsRosPublisher);
