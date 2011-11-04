1d5dcff6c4710a57de461ff34177a15b63fe752b
Amy Blank <amyblank@users.noreply.github.com>
2011-11-04 00:23:09 -0400

    removing monolithic cisst_ros

--- stacks/cisst_ros/cisst_ros_integration/src/ros_tasks.cpp
+++
@@ -1,8 +0,0 @@
-
-#include <cisstCommon/cmnConstants.h>
-#include "cisst_ros_integration/ros_tasks.h"
-
-// required to implement the class services, see cisstCommon
-CMN_IMPLEMENT_SERVICES(mtsRosTaskPeriodic);
-CMN_IMPLEMENT_SERVICES(mtsRosTaskFromSignal);
-CMN_IMPLEMENT_SERVICES(mtsRosTaskFromCallback);
